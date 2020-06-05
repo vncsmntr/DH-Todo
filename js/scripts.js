a7448c730aef9e44721605cb709c6fc993485bb9
Vinícius Monteiro <59066525+vncsmntr@users.noreply.github.com>
2020-06-05 13:24:01 -0300

    Reup
    Estou reupando meus projetos para manter o GitHub organizado | 
    I'm reupload my projects together to keep GitHub organized

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+let todoItems = [];
+
+
+// função adiciona todo
+function addTodo(text){
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    }
+
+    todoItems.push(todo);
+    console.log('LISTA - ', todoItems);
+
+// Ref UL HTML
+    const list = document.querySelector('.js-todo-list')
+    list.insertAdjacentHTML('beforeend', `
+        <li class="todo-item" data-key="${todo.id}">
+            <input id="${todo.id}" type="checkbox" />
+            <label for="${todo.id}" class="tick js-tick"></label>
+            <span>${todo.text}</span>
+            <button class="delete-todo js-delete-todo">
+                <svg>
+                    <use href="#delete-icon"></use>
+                </svg>
+            </button>    
+        </li>
+    `);
+}
+
+function checkTodo(key){
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    todoItems[index].checked = !todoItems[index].checked;
+
+    const item = document.querySelector(`[data-key='${key}']`);
+    if(todoItems[index].checked){
+        item.classList.add('done');
+    }else {
+        item.classList.remove('done');
+    }
+
+}
+
+
+//verifica evento formulario e valor input
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.querySelector('.js-todo-input');
+
+    var text = input.value.trim();
+    if (text !== ''){
+        addTodo(text);
+        input.value = '';
+        input.focus();
+    }
+})
+
+//verifica clique e item da lista para checar
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+    if(event.target.classList.contains('js-tick')){
+        const itemKey = event.target.parentElement.dataset.key;
+        checkTodo(itemKey)
+    }
+
+    if (event.target.classList.contains('js-delete-todo')){
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteTodo(itemKey);
+    }
+})
+
+function deleteTodo(key) {
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    const item = document.querySelector(`[data-key="${key}"]`);
+    item.remove();
+
+    const list = document.querySelector('.js-todo-list');
+    if (todoItems.length === 0) list.innerHTML = '';
+  
+}
